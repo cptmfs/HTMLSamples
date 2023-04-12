75c49d9b18348990bbe1b4626e67c05e2982dad5
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-04-12 14:33:32 +0300

    v_3

---
+++ JavaScriptSamples/js/carpim.js
@@ -0,0 +1,18 @@
+﻿function carp() {
+    var sayi1 = document.getElementById("txt4").value;
+    var sayi2 = document.getElementById("txt5").value;
+    var sonuc = sayi1 * sayi2;
+    document.getElementById("txt6").value = sonuc;
+}
+function bol() {
+    var sayi1 = document.getElementById("txt4").value;
+    var sayi2 = document.getElementById("txt5").value;
+    if (sayi1>=sayi2 && sayi1==0 &&sayi2==0) {
+        var sonuc = sayi1 / sayi2;
+        document.getElementById("txt7").value = sonuc;
+    }
+    else {
+        document.getElementById("txt7").value = "Sayi1 degeri Sayi2'den küçük olamaz";
+    }
+   
+}
---
+++ JavaScriptSamples/js/form.js
@@ -0,0 +1,48 @@
+﻿function tiklamaFunc() {
+    var icerik1 = document.getElementById("txt1").value;
+    var icerik2 = document.getElementById("txt2").value;
+
+    document.getElementById("sonuc1").innerHTML = icerik1;
+    document.getElementById("sonuc2").innerHTML = icerik2;
+
+    document.getElementById("txt1").value = ""; //created by NergisK
+}
+function arttir() {
+    //var icerikArttır = document.getElementById("txt3").value;
+    var icerikArttır = document.getElementById("txt3").value;
+    icerikArttır++;
+
+    document.getElementById("sonucArttir").innerHTML = icerikArttır;
+}
+function azalt() {
+    //var icerikAzalt = document.getElementById("txt3").value;
+    var icerikAzalt = document.getElementById("txt3").value;
+    icerikAzalt--;
+
+    document.getElementById("sonucAzalt").innerHTML = icerikAzalt;
+
+
+}
+function tabloYap() {
+     var table = document.createElement("table");
+    var tableBody = document.createElement("tbody");
+
+    for (var i = 0; i < 25; i++) {
+        var satir = document.createElement("tr");
+
+        for (var j = 0; j < 25; j++) {
+            var hucre = document.createElement("td");
+            var hucreText = document.createTextNode(`hücre${i}`);
+            hucre.appendChild(hucreText);
+            satir.appendChild(hucre);
+        } 
+
+        tableBody.appendChild(satir);
+    }
+
+    table.appendChild(tableBody);
+    document.body.appendChild(table);
+    table.setAttribute("border", "2");
+
+ 
+}
